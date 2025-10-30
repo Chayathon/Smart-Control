fc4dd93fc29e195f57f5fda6a8a099cc3440247d
Chayathon <gusgus157903@gmail.com>
2025-10-30 10:14:31 +0700

    Update song_screen

--- frontend/lib/routes/app_pages.dart
+++ frontend/lib/routes/app_pages.dart
@@ -3,7 +3,7 @@ import 'package:smart_control/mic.dart';
 import 'package:smart_control/screens/home_screen.dart';
 import 'package:smart_control/screens/login/login_screen.dart';
 import 'package:smart_control/screens/playlist/playlist_screen.dart';
-import 'package:smart_control/screens/song_upload/song_upload_screen.dart';
+import 'package:smart_control/screens/song/song_screen.dart';
 import 'package:smart_control/screens/splash_screen/splash_screen.dart';
 
 import 'app_routes.dart';
