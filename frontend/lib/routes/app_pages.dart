59babc42dae28c5a27275d4cbcf954a10be7e8d1
Chayathon <gusgus157903@gmail.com>
2025-11-07 16:54:12 +0700

    Update routes

--- frontend/lib/routes/app_pages.dart
+++ frontend/lib/routes/app_pages.dart
@@ -3,7 +3,8 @@ import 'package:smart_control/mic.dart';
 import 'package:smart_control/screens/home_screen.dart';
 import 'package:smart_control/screens/login/login_screen.dart';
 import 'package:smart_control/screens/playlist/playlist_screen.dart';
-import 'package:smart_control/screens/settings/setting_screen.dart';
+import 'package:smart_control/screens/settings/schedule/schedule_screen.dart';
+import 'package:smart_control/screens/settings/system/system_screen.dart';
 import 'package:smart_control/screens/song/song_screen.dart';
 import 'package:smart_control/screens/splash_screen/splash_screen.dart';
 import 'package:smart_control/screens/monitoring/monitoring_screen.dart';
@@ -17,7 +18,8 @@ class AppPages {
     GetPage(name: AppRoutes.playlist, page: () => const PlaylistScreen()),
     GetPage(name: AppRoutes.song, page: () => const SongScreen()),
     GetPage(name: AppRoutes.monitoring, page: () => const MonitoringScreen()),
-    GetPage(name: AppRoutes.settings, page: () => const SettingScreen()),
+    GetPage(name: AppRoutes.system, page: () => const SystemScreen()),
+    GetPage(name: AppRoutes.schedule, page: () => const ScheduleScreen()),
     GetPage(name: AppRoutes.splash, page: () => const SplashScreen()),
     GetPage(name: AppRoutes.test, page: () => const MicPage()),
   ];
--- frontend/lib/routes/app_routes.dart
+++ frontend/lib/routes/app_routes.dart
@@ -4,7 +4,8 @@ abstract class AppRoutes {
   static const playlist = '/playlist';
   static const song = '/song';
   static const monitoring = '/monitoring';
-  static const settings = '/settings';
+  static const system = '/settings/system';
+  static const schedule = '/settings/schedule';
   static const splash = '/splash';
   static const test = "/test";
 }
