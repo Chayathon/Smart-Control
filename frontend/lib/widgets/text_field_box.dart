28ef5acbe324301b22151c74d5677b7e17934014
Chayathon <gusgus157903@gmail.com>
2025-11-17 16:29:04 +0700

    Create text_field_box.dart

---
+++ frontend/lib/widgets/text_field_box.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+/// Reusable TextField Widget with consistent styling
+class TextFieldBox extends StatelessWidget {
+  const TextFieldBox({
+    Key? key,
+    required this.controller,
+    required this.hint,
+    this.textInputAction,
+    this.keyboardType,
+    this.obscureText = false,
+    this.maxLines = 1,
+    this.enabled = true,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onChanged,
+    this.validator,
+    this.fillColor,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final String hint;
+  final TextInputAction? textInputAction;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final int maxLines;
+  final bool enabled;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final Color? fillColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      textInputAction: textInputAction ?? TextInputAction.next,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      maxLines: maxLines,
+      enabled: enabled,
+      onChanged: onChanged,
+      validator: validator,
+      decoration: InputDecoration(
+        hintText: hint,
+        filled: true,
+        fillColor: fillColor ?? Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.blue, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.red, width: 2),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.red, width: 2),
+        ),
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      ),
+      style: TextStyle(fontSize: 14),
+    );
+  }
+}
