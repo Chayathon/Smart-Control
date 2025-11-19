6c2ae5622c4f0dad0ace98883ec33e25601b3acd
Chayathon <gusgus157903@gmail.com>
2025-11-19 15:42:09 +0700

    Update stream.routes.js

--- backend/routes/stream.routes.js
+++ backend/routes/stream.routes.js
@@ -3,21 +3,16 @@ const ctrl = require('../controllers/stream.controller');
 const { authenticateToken } = require('../middleware/auth');
 
 router.get('/status', ctrl.status);
-
-// Mic control endpoint
-router.post('/mic/stop', authenticateToken, ctrl.stopMic);
-
 router.post('/enable', authenticateToken, ctrl.enableStream);
 router.post('/disable', authenticateToken, ctrl.disableStream);
 
-router.get('/start-playlist', authenticateToken, ctrl.playPlaylist);
-router.get('/stop', authenticateToken, ctrl.stopAll);
-router.get('/next-track', authenticateToken, ctrl.nextTrack);
-router.get('/prev-track', authenticateToken, ctrl.prevTrack);
-router.get('/pause', authenticateToken, ctrl.pause);
-router.get('/resume', authenticateToken, ctrl.resume);
-
 router.get('/start-file', ctrl.startFile);
 router.get('/start-youtube', ctrl.startYoutube);
+router.get('/start-playlist', authenticateToken, ctrl.startPlaylist);
+router.get('/pause', authenticateToken, ctrl.pause);
+router.get('/resume', authenticateToken, ctrl.resume);
+router.get('/next-track', authenticateToken, ctrl.nextTrack);
+router.get('/prev-track', authenticateToken, ctrl.prevTrack);
+router.get('/stop', authenticateToken, ctrl.stopAll);
 
 module.exports = router;
