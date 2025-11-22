3658507aab80463ecccc2df1e6ad3bf04bf33e8c
Sam <k.jakkrit2001@gmail.com>
2025-11-22 16:11:33 +0700

    Update radioZone.service.js

--- backend/services/radioZone.service.js
+++ backend/services/radioZone.service.js
@@ -226,8 +226,8 @@ function onRxFrame(frameBuf) {
 
   if (type === 'stream') {
     const topicStatus = isAll
-      ? 'mass-radio/all/status'
-      : `mass-radio/zone${zone}/status`;
+      ? 'mass-radio/all/command'
+      : `mass-radio/zone${zone}/command`;
 
     const payloadStatus = {
       zone,
